23ae05be0dba7f1f2df41d8e083ca8dca607bff1
KIM Taegyoon <steloflute@gmail.com>
2017-06-04 04:26:25 +0900

    Add files via upload

---
+++ predict-mnist2.go
@@ -0,0 +1,125 @@
+// squared error
+// accuracy: 0.9691
+
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"strings"
+	"os"
+	"log"
+	"strconv"
+	"math"
+)
+
+func square(x float64) float64 {
+	return x * x
+}
+
+func correl(x, y []float64) float64 {
+	sumX := 0.0
+	sumY := 0.0
+	l := len(x)
+	if l != len(y) {
+		log.Fatal("not same size")
+	}
+	for i := range x {
+		sumX += x[i]
+		sumY += y[i]
+	}
+	meanX := sumX / float64(l)
+	meanY := sumY / float64(l)
+	varX := 0.0
+	varY := 0.0
+	sumProdDiff := 0.0
+	for i := range x {
+		sumProdDiff += (x[i] - meanX) * (y[i] - meanY)
+		varX += square(x[i] - meanX)
+		varY += square(y[i] - meanY)
+	}
+	stdevX := math.Pow(varX, 0.5)
+	stdevY := math.Pow(varY, 0.5)
+	return sumProdDiff / stdevX / stdevY
+}
+
+func sumSqErr(x, y []float64) float64 {
+	l := len(x)
+	if l != len(y) {
+		log.Fatal("not same size")
+	}
+	sum := 0.0
+	for i := range x {
+		sum += square(x[i] - y[i])
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println("!")
+	const fileNameTrain = "mnist_train.csv"
+	const fileNameTest = "mnist_test.csv"
+	
+	// train
+	file, err := os.Open(fileNameTrain)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sin := bufio.NewScanner(file)
+	trainData := [][]float64{}
+	for sin.Scan() {
+		line := sin.Text()
+		split := strings.Split(line, ",")
+		data := []float64{}
+		for _, v := range split {
+			n, err := strconv.ParseFloat(v, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			data = append(data, n)
+		}
+		trainData = append(trainData, data)
+	}
+	file.Close()
+	fmt.Println("train data loaded. rows:", len(trainData))
+
+	// test
+	file, err = os.Open(fileNameTest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sin = bufio.NewScanner(file)
+	nRows := 0
+	nCorrect := 0
+	for sin.Scan() {
+		nRows++
+		line := sin.Text()
+		split := strings.Split(line, ",")
+		data := []float64{}
+		for _, v := range split {
+			n, err := strconv.ParseFloat(v, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			data = append(data, n)
+		}
+		// fmt.Println("data:", data)
+
+		// predict
+		best := 0
+		minError := math.MaxFloat64
+		for i, d := range trainData {
+			error := sumSqErr(data[1:], d[1:])
+			if error < minError {
+				minError = error
+				best = int(trainData[i][0])
+			}
+		}
+		if best == int(data[0]) {
+			nCorrect++
+		}
+		fmt.Println("predicted:", best, "answer:", data[0], "accuracy:", float64(nCorrect) / float64(nRows))
+	}
+	file.Close()
+	fmt.Println("train data loaded. rows:", nRows, "accuracy:", float64(nCorrect) / float64(nRows))
+}
